bc451fa7ee6b7c5280c6711cdbc3d3f816aded15
=noloh <info@noloh.com>
2011-02-11 02:34:41 -0500

    Documented all Verbs in accordance with twilio documentation in standard phpdoc format for IDE autocomplete compatibility

--- twilio.php
+++ twilio.php
@@ -330,7 +330,6 @@
         
     }
     
-    
     class Response extends Verb {
         
         private $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response></Response>";
@@ -367,61 +366,219 @@
         }
         
     }
-    
+    /**
+    * The <Say> verb converts text to speech that is read back to the caller.
+    * <Say> is useful for development or saying dynamic text that is difficult to pre-record.
+    */
     class Say extends Verb {
     
         protected $valid = array('voice','language','loop');
-    
+        /**
+        * Say Constructor
+        * 
+        * Instatiates a new Say object with text and optional attributes.
+        * Possible attributes are:
+        * 	"voice" => 'man'|'woman',
+        *   "language" => 'en'|'es'|'fr'|'de',
+        * 	"loop"	=> integer >= 0
+        *
+        * @param string $text
+        * @param array $attr Optional attributes
+        * @return Say
+        */
+        function __construct($text='', $attr = array()) {
+			parent::__construct($text, $attr);
+        }
     }
-
+	/**
+	* The <Reject> verb rejects an incoming call to your Twilio number without 
+	* billing you. This is very useful for blocking unwanted calls.
+	* If the first verb in a TwiML document is <Reject>, Twilio will not pick 
+	* up the call. The call ends with a status of 'busy' or 'no-answer',
+	* depending on the verb's 'reason' attribute. Any verbs after 
+	* <Reject> are unreachable and ignored.
+	* 
+	* Note that using <Reject> as the first verb in your response is the only 
+	* way to prevent Twilio from answering a call. Any other response will 
+	* result in an answered call and your account will be billed.
+	*/
 	class Reject extends Verb {
 		
 		protected $valid = array('reason');
-			
+		
+        /**
+        * Reject Constructor
+        * 
+        * Instatiates a new Reject object with optional attributes.
+        * Possible attributes are:
+        * 	"reason" => 'rejected'|'busy',
+        *
+        * @param array $attr Optional attributes, defaults to 'rejected'
+        * @return Reject
+        */
+        function __construct($attr = array()) {
+			parent::__construct($attr);
+        }		
 	}
-    
+    /**
+    * The <Play> verb plays an audio file back to the caller. 
+    * Twilio retrieves the file from a URL that you provide.
+    */
     class Play extends Verb {
         
         protected $valid = array('loop');
-    
+        
+    	/**
+        * Play Constructor
+        * 
+        * Instatiates a new Play object with a URL and optional attributes.
+        * Possible attributes are:
+        * 	"loop" =>  integer >= 0
+        *
+        * @param string $url The URL of an audio file that Twilio will retrieve and play to the caller.
+        * @param array $attr Optional attributes
+        * @return Play
+        */
+        function __construct($url='', $attr = array()) {
+			parent::__construct($url, $attr);
+        } 
     }
     
-    
+    /**
+    * The <Record> verb records the caller's voice and returns to you the URL 
+    * of a file containing the audio recording. You can optionally generate 
+    * text transcriptions of recorded calls by setting the 'transcribe' 
+    * attribute of the <Record> verb to 'true'.
+    */
     class Record extends Verb {
     
         protected $valid = array('action','method','timeout','finishOnKey',
 								 'maxLength','transcribe','transcribeCallback', 'playBeep');
-    
+		
+		/**
+        * Record Constructor
+        * 
+        * Instatiates a new Record object with optional attributes.
+        * Possible attributes are:
+        * 	"action" =>  relative or absolute url, (default: current url)
+        * 	"method" => 'GET'|'POST', (default: POST)
+        *   "timeout" => positive integer, (default: 5)
+        * 	"finishOnKey"	=> any digit, #, * (default: 1234567890*#)
+        * 	"maxLength"	=> integer >= 1, (default: 3600, 1hr)
+        * 	"transcribe" => true|false, (default: false)
+        * 	"transcribeCallback" => relative or absolute url
+        * 	"playBeep" => true|false, (default: true)
+        *
+        * @param array $attr Optional attributes
+        * @return Record
+        */
+        function __construct($attr = array()) {
+			parent::__construct($attr);
+        } 
     }
     
-    
+    /**
+    * The <Dial> verb connects the current caller to an another phone. 
+    * If the called party picks up, the two parties are connected and can 
+    * communicate until one hangs up. If the called party does not pick up,
+    *  if a busy signal is received, or if the number doesn't exist, 
+    * the dial verb will finish. 
+    * 
+    * When the dialed call ends, Twilio makes a GET or POST request to 
+    * the 'action' URL if provided. Call flow will continue using 
+    * the TwiML received in response to that request.
+    */
     class Dial extends Verb {
     
         protected $valid = array('action','method','timeout','hangupOnStar',
             'timeLimit','callerId');
     
         protected $nesting = array('Number','Conference');
+        
+        /**
+        * Dial Constructor
+        * 
+        * Instatiates a new Dial object with a number and optional attributes.
+        * Possible attributes are:
+        * 	"action" =>  relative or absolute url
+        * 	"method" => 'GET'|'POST', (default: POST)
+        *   "timeout" => positive integer, (default: 30)
+        * 	"hangupOnStar"	=> true|false, (default: false)
+        * 	"timeLimit"	=> integer >= 0, (default: 14400, 4hrs)
+        * 	"callerId" => valid phone #, (default: Caller's callerid)
+        *
+        * @param string|Number|Conference $number The number or conference you wish to call
+        * @param array $attr Optional attributes
+        * @return Dial
+        */
+        function __construct($number='', $attr = array()) {
+			parent::__construct($number, $attr);
+        } 
     
     }
-    
+    /**
+    * The <Redirect> verb transfers control of a call to the TwiML at a 
+    * different URL. All verbs after <Redirect> are unreachable and ignored.
+    */
     class Redirect extends Verb {
     
         protected $valid = array('method');
+        
+        /**
+        * Redirect Constructor
+        * 
+        * Instatiates a new Redirect object with text and optional attributes.
+        * Possible attributes are:
+        * 	"method" => 'GET'|'POST', (default: POST)
+        *
+        * @param string $url An absolute or relative URL for a different TwiML document.
+        * @param array $attr Optional attributes
+        * @return Redirect
+        */
+        function __construct($url='', $attr = array()) {
+			parent::__construct($url, $attr);
+        } 
     
     }
-    
+    /**
+    * The <Pause> verb waits silently for a specific number of seconds. 
+    * If <Pause> is the first verb in a TwiML document, Twilio will wait 
+    * the specified number of seconds before picking up the call.
+    */
     class Pause extends Verb {
     
         protected $valid = array('length');
     
+    	/**
+        * Pause Constructor
+        * 
+        * Instatiates a new Pause object with text and optional attributes.
+        * Possible attributes are:
+        * 	"length" => integer > 0, (default: 1)
+        *
+        * @param array $attr Optional attributes
+        * @return Pause
+        */
         function __construct($attr = array()) {
             parent::__construct(NULL, $attr);
         }
     
     }
-    
+    /**
+    * The <Hangup> verb ends a call. If used as the first verb in a TwiML 
+    * response it does not prevent Twilio from answering the call and billing 
+    * your account. The only way to not answer a call and prevent billing 
+    * is to use the <Reject> verb.
+    */
     class Hangup extends Verb {
     
+    	/**
+        * Hangup Constructor
+        * 
+        * Instatiates a new Hangup object.
+        * 
+        * @return Hangup
+        */
         function __construct() {
             parent::__construct(NULL, array());
         }
@@ -429,35 +586,138 @@
     
     }
     
-    
+    /**
+    * The <Gather> verb collects digits that a caller enters into his or her 
+    * telephone keypad. When the caller is done entering data, Twilio submits 
+    * that data to the provided 'action' URL in an HTTP GET or POST request, 
+    * just like a web browser submits data from an HTML form.
+    * If no input is received before timeout, <Gather> falls through to the 
+    * next verb in the TwiML document. 
+    * 
+    * You may optionally nest <Say> and <Play> within a <Gather> verb while 
+    * waiting for input. This allows you to read menu options to the caller 
+    * while letting her enter a menu selection at any time. After the first 
+    * digit is received the audio will stop playing.
+    */
     class Gather extends Verb {
     
         protected $valid = array('action','method','timeout','finishOnKey',
             'numDigits');
             
         protected $nesting = array('Say', 'Play', 'Pause');
-        
+        /**
+        * Gather Constructor
+        * 
+        * Instatiates a new Gather object with optional attributes.
+        * Possible attributes are:
+        * 	"action" =>  relative or absolute url (default: current url)
+        * 	"method" => 'GET'|'POST', (default: POST)
+        *   "timeout" => positive integer, (default: 5)
+        * 	"finishOnKey"	=> any digit, #, *, (default: #)
+        * 	"numDigits"	=> integer >= 1 (default: unlimited)
+        *
+        * @param array $attr Optional attributes
+        * @return Gather
+        */
         function __construct($attr = array()){
             parent::__construct(NULL, $attr);
         }
     
     }
-    
+    /**
+    * The <Dial> verb's <Number> noun specifies a phone number to dial. 
+    * Using the noun's attributes you can specify particular behaviors 
+    * that Twilio should apply when dialing the number.
+    * 
+    * You can use multiple <Number> nouns within a <Dial> verb to simultaneously
+    *  call all of them at once. The first call to pick up is connected 
+    * to the current call and the rest are hung up.
+    */
     class Number extends Verb {
     
         protected $valid = array('url','sendDigits');
+        
+         /**
+        * Number Constructor
+        * 
+        * Instatiates a new Number object with optional attributes.
+        * Possible attributes are:
+        * 	"sendDigits"	=> any digits
+        * 	"url"	=> any url
+        *
+        * @param string $number Number you wish to dial
+        * @param array $attr Optional attributes
+        * @return Number
+        */
+         function __construct($number = '', $attr = array()){
+            parent::__construct($number, $attr);
+         }
             
     }
-    
+    /**
+    * The <Dial> verb's <Conference> noun allows you to connect to a conference
+    * room. Much like how the <Number> noun allows you to connect to another
+    * phone number, the <Conference> noun allows you to connect to a named 
+    * conference room and talk with the other callers who have also connected 
+    * to that room.
+    * 
+    * The name of the room is up to you and is namespaced to your account. 
+    * This means that any caller who joins 'room1234' via your account will 
+    * end up in the same conference room, but callers connecting through 
+    * different accounts would not. The maximum number of participants in a 
+    * single Twilio conference room is 40.
+    */
     class Conference extends Verb {
     
         protected $valid = array('muted','beep','startConferenceOnEnter',
             'endConferenceOnExit','waitUrl','waitMethod');
+        
+        /**
+        * Conference Constructor
+        * 
+        * Instatiates a new Conference object with room and optional attributes.
+        * Possible attributes are:
+        * 	"muted"	=> true|false, (default: false)
+        * 	"beef"	=> true|false, (default: true)
+        * 	"startConferenceOnEnter"	=> true|false (default: true)
+        * 	"endConferenceOnExit"	=> true|false (default: false)
+        * 	"waitUrl"	=> TwiML url, empty string,	(default: Twilio hold music)
+        * 	"waitMethod"	=> 'GET'|'POST', (default: POST)
+        * 	"maxParticipants"	=> integer > 0 && <= 40 (default: 40)
+        *
+        * @param string $room Conference room to join
+        * @param array $attr Optional attributes
+        * @return Conference
+        */
+         function __construct($room = '', $attr = array()){
+            parent::__construct($room, $attr);
+         }
             
     }
-    
+    /**
+    * The <Sms> verb sends an SMS message to a phone number during a phone call.
+    */
     class Sms extends Verb {
         protected $valid = array('to', 'from', 'action', 'method', 'statusCallback');
+        
+        /**
+        * SMS Constructor
+        * 
+        * Instatiates a new SMS object with room and optional attributes.
+        * Possible attributes are:
+        * 	"to"	=> phone #
+        * 	"from"	=> sms capable phone #
+        * 	"action"	=> true|false (default: true)
+        * 	"method"	=> 'GET'|'POST', (default: POST)
+        * 	"statusCallback"	=> relative or absolute URL
+        *
+        * @param string $message SMS message to send
+        * @param array $attr Optional attributes
+        * @return SMS
+        */
+         function __construct($message = '', $attr = array()){
+            parent::__construct($message, $attr);
+         }
     }
     
     // Twilio Utility function and Request Validation
