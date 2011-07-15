e919fd7882fc9507c463843713aba5983286509f
Neuman <neuman@ashmoremusic.com>
2011-07-14 19:02:52 -0700

    Add test for DELETE on notifications

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -96,3 +96,20 @@ class ApplicationsTest extends PHPUnit_Framework_TestCase
         m::close();
     }
 }
+
+class NotificationTest extends PHPUnit_Framework_TestCase
+{
+    function testDelete() {
+        $http = m::mock();
+        $http->shouldReceive('delete')->once()
+            ->with('/2010-04-01/Accounts/AC123/Notifications/NO123.json')
+            ->andReturn(array(204, array(), ''));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $client->account->notifications->delete('NO123');
+    }
+
+    function tearDown()
+    {
+        m::close();
+    }
+}
