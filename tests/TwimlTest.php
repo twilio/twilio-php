dd7a27a1ad8a947c61f959a0412a8fb4b0c7f1e0
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-07-19 13:08:58 -0700

    Update the package.xml to remove unused dependencies

--- tests/TwimlTest.php
+++ tests/TwimlTest.php
@@ -1,15 +1,11 @@
 <?php
 
-use \Mockery as m;
-
 require_once 'Twilio/Twiml.php';
 
 class TwimlTest extends PHPUnit_Framework_TestCase {
-    function tearDown() {
-        m::close();
-    }
 
-    function testStuff() {
+    function testGeneration() {
+
         $r = new Services_Twilio_Twiml();
         $r->say('hello');
         $r->dial()->number('123', array('sendDigits' => '456'));
@@ -17,9 +13,9 @@ class TwimlTest extends PHPUnit_Framework_TestCase {
 
         $doc = simplexml_load_string($r);
         $this->assertEquals('Response', $doc->getName());
-        $this->assertEquals('hello', (string)$doc->Say);
-        $this->assertEquals('456', (string)$doc->Dial->Number['sendDigits']);
-        $this->assertEquals('123', (string)$doc->Dial->Number);
-        $this->assertEquals('15', (string)$doc->Gather['timeout']);
+        $this->assertEquals('hello', (string) $doc->Say);
+        $this->assertEquals('456', (string) $doc->Dial->Number['sendDigits']);
+        $this->assertEquals('123', (string) $doc->Dial->Number);
+        $this->assertEquals('15', (string) $doc->Gather['timeout']);
     }
 }
