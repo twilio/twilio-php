2905d5b47b00f1762fbde821416a2d1802354652
Kevin Burke <kev@inburke.com>
2012-01-06 01:43:51 -0800

    bump year in copyright notice

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -41,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Services_Twilio'
-copyright = u'2011, Twilio Inc.'
+copyright = unicode(datetime.utcnow().year) + u', Twilio Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
