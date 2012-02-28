434b7115394fd5b7dd5b0233b2508d550dfec7a5
Kevin Burke <kevin@twilio.com>
2012-02-28 10:18:21 -0800

    add mockery channel to preinstall

--- travis_install.sh
+++ travis_install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 hash pyrus 2>&- && {
+    pyrus channel-discover pear.survivethedeepend.com
     pyrus channel-discover twilio.github.com/pear
     pyrus install twilio/Services_Twilio --optionaldeps
 }
 
 hash pear 2>&- && {
+    pear channel-discover pear.survivethedeepend.com
     pear channel-discover twilio.github.com/pear
-    pear install twilio/Services_Twilio --optionaldeps
+    pear install twilio/Services_Twilio --alldeps
 }
