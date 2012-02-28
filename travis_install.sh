bdafacf4e786f3c8548e06b3513cf7f5b71081db
Kevin Burke <kevin@twilio.com>
2012-02-28 10:50:05 -0800

    test hash function with command.

--- travis_install.sh
+++ travis_install.sh
@@ -3,6 +3,9 @@
 echo `which pyrus`
 echo `which pear`
 
+hash pyrus 2>&- || { echo "Pyrus is not installed."; }
+hash pear 2>&- || { echo "pear is not installed."; }
+
 hash pyrus 2>&- && {
     pyrus channel-discover pear.survivethedeepend.com
     pyrus channel-discover twilio.github.com/pear
