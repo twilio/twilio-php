609db4204b6237649e4ff14ca126c0c1d525f2ca
Kevin Burke <kevin@twilio.com>
2012-02-28 10:13:35 -0800

    try pear.

--- travis_install.sh
+++ travis_install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+hash pyrus 2>&- && {
 pyrus channel-discover twilio.github.com/pear
 pyrus install twilio/Services_Twilio
+}
+
+pear channel-discover twilio.github.com/pear
+pear install twilio/Services_Twilio
