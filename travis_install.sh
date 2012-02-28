086e7c8cd0d88a9651ebeb90e0c3a6a18a2dfd4e
Kevin Burke <kevin@twilio.com>
2012-02-28 11:00:29 -0800

    check for php version

--- travis_install.sh
+++ travis_install.sh
@@ -3,19 +3,14 @@
 echo `which pyrus`
 echo `which pear`
 
-hash pyrus 2>&- || { echo "Pyrus is not installed."; }
-hash pear 2>&- || { echo "pear is not installed."; }
-
-hash pyrus 2>&- && {
+if [ "$TRAVIS_PHP_VERSION" == "5.2" ]; then
+    pear channel-discover pear.survivethedeepend.com
+    pear channel-discover twilio.github.com/pear
+    pear install --alldeps twilio/Services_Twilio
+else
     pyrus channel-discover pear.survivethedeepend.com
     pyrus channel-discover twilio.github.com/pear
     pyrus install twilio/Services_Twilio --optionaldeps
-}
-
-hash pear 2>&- && {
-    pear channel-discover pear.survivethedeepend.com
-    pear channel-discover twilio.github.com/pear
-    pear install twilio/Services_Twilio --alldeps
-}
+fi
 
 phpenv rehash
