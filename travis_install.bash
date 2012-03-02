b40a66353062e9a6c2f729338d4470e59b6b6477
Kevin Burke <kevin@twilio.com>
2012-03-01 20:10:58 -0800

    remove 5.2 as the tests don't work.

--- travis_install.bash
+++ travis_install.bash
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_PHP_VERSION" == "5.2" ]]; then
-    pear channel-discover pear.survivethedeepend.com
-    pear channel-discover twilio.github.com/pear
-    pear install deepend/Mockery
-    pear install --alldeps twilio/Services_Twilio
-else
-    pyrus channel-discover pear.survivethedeepend.com
-    pyrus channel-discover twilio.github.com/pear
-    pyrus install deepend/Mockery
-    pyrus install twilio/Services_Twilio --optionaldeps
-fi
+pyrus channel-discover pear.survivethedeepend.com
+pyrus channel-discover twilio.github.com/pear
+pyrus install deepend/Mockery
+pyrus install twilio/Services_Twilio --optionaldeps
 
 phpenv rehash
 
