1b3a6a5cf275d6bc45a1ae917282f862330d7fd9
Kevin Burke <kevin@twilio.com>
2012-06-03 11:06:49 -0700

    Make the tests pass.

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -114,7 +114,7 @@ class ApplicationsTest extends PHPUnit_Framework_TestCase
 
 class ConnectAppsTest extends PHPUnit_Framework_TestCase
 {
-    function testUpdate()
+    function testUpdateWithArray()
     {
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('get')->once()
@@ -127,12 +127,34 @@ class ConnectAppsTest extends PHPUnit_Framework_TestCase
             array('Content-Type' => 'application/x-www-form-urlencoded'), 
             'FriendlyName=Bar')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
-                json_encode(array('friendly_name' => 'Boo'))
+                json_encode(array('friendly_name' => 'Bar'))
             ));
         $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
         $cn = $client->account->connect_apps->get('CN123');
         $this->assertEquals('foo', $cn->friendly_name);
-        $cn->update('Bar', array('friendly_name' => 'Bar'));
+        $cn->update(array('FriendlyName' => 'Bar'));
+        $this->assertEquals('Bar', $cn->friendly_name);
+    }
+
+    function testUpdateWithOneParam()
+    {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/ConnectApps/CN123.json')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('friendly_name' => 'foo'))
+            ));
+        $http->shouldReceive('post')->once()
+            ->with('/2010-04-01/Accounts/AC123/ConnectApps/CN123.json', 
+            array('Content-Type' => 'application/x-www-form-urlencoded'), 
+            'FriendlyName=Bar')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('friendly_name' => 'Bar'))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $cn = $client->account->connect_apps->get('CN123');
+        $this->assertEquals('foo', $cn->friendly_name);
+        $cn->update('FriendlyName', 'Bar');
         $this->assertEquals('Bar', $cn->friendly_name);
     }
     
