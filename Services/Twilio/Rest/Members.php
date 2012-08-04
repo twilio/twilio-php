27b6ca77402577e77219b1b8e8960144efa319ee
Kevin Burke <kevin@twilio.com>
2012-08-04 12:14:26 +0100

    Finish implementation of Queue
    Add support for dequeuing members and creating new Queues. Document an existing
    method.

--- Services/Twilio/Rest/Members.php
+++ Services/Twilio/Rest/Members.php
@@ -3,6 +3,11 @@
 class Services_Twilio_Rest_Members
     extends Services_Twilio_ListResource
 {
+    /**
+     * Return the member at the front of the queue.
+     *
+     * @return Services_Twilio_Rest_Member The member at the front of the queue
+     */
     public function front() {
         return new $this->instance_name($this->client, $this->uri . "/Front");
     }
