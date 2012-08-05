16f0a93484412ce1fa462b0398d09b0cd4753d4e
Kevin Burke <kevin@twilio.com>
2012-08-05 23:38:32 +0100

    Add comments and fix formatting

--- Services/Twilio/Rest/Queue.php
+++ Services/Twilio/Rest/Queue.php
@@ -1,13 +1,10 @@
 <?php
 
 class Services_Twilio_Rest_Queue
-    extends Services_Twilio_InstanceResource
-{
-    protected function init($client, $uri)
-    {
-        $this->setupSubresources(
-            'members'
-        );
+    extends Services_Twilio_InstanceResource {
+
+    protected function init($client, $uri) {
+        $this->setupSubresources('members');
     }
 }
 
