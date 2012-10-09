ab925b993de82457f38346f8455cfdb50343775d
Kevin Burke <kevin@twilio.com>
2012-10-08 22:19:02 -0700

    add another test

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -425,4 +425,38 @@ class RecordsTest extends PHPUnit_Framework_TestCase {
         $this->assertSame('100.30', $callRecord->price);
     }
 
+    function testFilterUsageRecords() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $params = 'Page=0&PageSize=50&StartDate=2012-08-01&EndDate=2012-08-31';
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/Usage/Records.json?' . $params)
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('usage_records' => array(
+                    array(
+                        'category' => 'sms',
+                        'count' => 4,
+                        'price' => '300.30',
+                    ),
+                    array(
+                        'category' => 'calls',
+                        'count' => 4,
+                        'price' => '100.30',
+                    )),
+                ))
+            ));
+        $params = 'Page=1&PageSize=50&StartDate=2012-08-01&EndDate=2012-08-31';
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/Usage/Records.json?' . $params)
+            ->andReturn(array(400, array('Content-Type' => 'application/json'),
+                '{"status":400,"message":"foo", "code": "20006"}'
+            ));
+        $client = new Services_Twilio('AC123', '456bef', '2010-04-01', $http);
+        foreach ($client->account->usage_records->getIterator(0, 50, array(
+            'StartDate' => '2012-08-01',
+            'EndDate'   => '2012-08-31',
+        )) as $record) {
+            $this->assertSame(4, $record->count);
+        }
+    }
 }
+
