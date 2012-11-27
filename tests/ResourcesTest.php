2ea84be33aee1a68cbdd9a3f34d183fd0002520b
Kevin Burke <kevin@twilio.com>
2012-11-26 21:28:35 -0800

    fix test

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -359,15 +359,22 @@ class MembersTest extends PHPUnit_Framework_TestCase {
 
     function testMemberIterate() {
         $http = m::mock(new Services_Twilio_TinyHttp);
-        $http->shouldReceive('get')->once()
-            ->with('/2010-04-01/Accounts/AC123/Queues/QQ123/Members.json?Page=0&PageSize=50')
-            ->andReturn(array(200, array('Content-Type' => 'application/json'),
-                json_encode(
+        $resp = json_encode(
                     array(
-                        'queue_members' => array('call_sid' => 'CA123', 'wait_time' => 30),
+                        'queue_members' => array(
+                            array('call_sid' => 'CA123', 'wait_time' => 30)
+                        ),
                         'end' => 1,
                     )
-                )
+                );
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/Queues/QQ123/Members.json?Page=0&PageSize=50')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'), $resp
+            ));
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/Queues/QQ123/Members.json?Page=1&PageSize=50')
+            ->andReturn(array(400, array('Content-Type' => 'application/json'),
+                '{"status":400,"message":"foo", "code": "20006"}'
             ));
         $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
         $queue = $client->account->queues->get('QQ123');
