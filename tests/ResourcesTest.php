55ed0b5adf97ad6b9356e1d42bb652a36d19b86e
Kevin Burke <kevin@twilio.com>
2012-12-14 15:47:32 -0800

    Add test for Shortcode resource retrieval

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -290,6 +290,20 @@ class SMSMessagesTest extends PHPUnit_Framework_TestCase
     }
 }
 
+class ShortCodesTest extends PHPUnit_Framework_TestCase {
+    function testShortcodeResource() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/SMS/ShortCodes/SC123.json')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('sid' => 'SM123', 'short_code' => '1234'))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $sms = $client->account->short_codes->get('SC123');
+        $this->assertSame('1234', $sms->short_code);
+    }
+}
+
 class CallsTest extends PHPUnit_Framework_TestCase
 {
     /**
