aeea70ac0be624f7b99fb240fc27df945e2010fa
Kevin Burke <kevin@twilio.com>
2012-12-14 15:53:46 -0800

    change short code sid to match prefix

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -291,12 +291,13 @@ class SMSMessagesTest extends PHPUnit_Framework_TestCase
 }
 
 class ShortCodesTest extends PHPUnit_Framework_TestCase {
+
     function testShortcodeResource() {
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('get')->once()
             ->with('/2010-04-01/Accounts/AC123/SMS/ShortCodes/SC123.json')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
-                json_encode(array('sid' => 'SM123', 'short_code' => '1234'))
+                json_encode(array('sid' => 'SC123', 'short_code' => '1234'))
             ));
         $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
         $sms = $client->account->short_codes->get('SC123');
