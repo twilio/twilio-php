b2509b818fdcc0e31336fd145dd62f55df01cb90
Kevin Burke <kevin@twilio.com>
2013-08-30 17:40:08 -0700

    s/MediaUrls/MediaUrl
    turns out the library itself is naive to the implementation

--- tests/resources/MessagesTest.php
+++ tests/resources/MessagesTest.php
@@ -23,13 +23,27 @@ class MessagesTest extends PHPUnit_Framework_TestCase
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('post')->once()
             ->with('/2010-04-01/Accounts/AC123/Messages.json', $this->formHeaders,
-                'From=%2B1222&To=%2B44123&MediaUrls=http%3A%2F%2Fexample.com%2Fimage1')
+                'From=%2B1222&To=%2B44123&MediaUrl=http%3A%2F%2Fexample.com%2Fimage1')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array('sid' => 'SM123'))
             ));
         $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
         $msg = $client->account->messages->create('+1222', '+44123',
-            array('MediaUrls' => array('http://example.com/image1')));
+            array('MediaUrl' => array('http://example.com/image1')));
+        $this->assertSame('SM123', $msg->sid);
+    }
+
+    function testMediaUrlNoArray() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('post')->once()
+            ->with('/2010-04-01/Accounts/AC123/Messages.json', $this->formHeaders,
+                'From=%2B1222&To=%2B44123&MediaUrl=http%3A%2F%2Fexample.com%2Fimage1')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('sid' => 'SM123'))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $msg = $client->account->messages->create('+1222', '+44123',
+            array('MediaUrl' => 'http://example.com/image1'));
         $this->assertSame('SM123', $msg->sid);
     }
 
@@ -37,7 +51,7 @@ class MessagesTest extends PHPUnit_Framework_TestCase
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('post')->once()
             ->with('/2010-04-01/Accounts/AC123/Messages.json', $this->formHeaders,
-                'From=%2B1222&To=%2B44123&Body=Hi+there&MediaUrls=http%3A%2F%2Fexample.com%2Fimage1')
+                'From=%2B1222&To=%2B44123&Body=Hi+there&MediaUrl=http%3A%2F%2Fexample.com%2Fimage1')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array('sid' => 'SM123'))
             ));
@@ -45,7 +59,7 @@ class MessagesTest extends PHPUnit_Framework_TestCase
         $msg = $client->account->messages->create('+1222', '+44123',
             array(
                 'Body' => 'Hi there',
-                'MediaUrls' => array('http://example.com/image1')
+                'MediaUrl' => array('http://example.com/image1')
             ));
         $this->assertSame('SM123', $msg->sid);
     }
@@ -54,13 +68,13 @@ class MessagesTest extends PHPUnit_Framework_TestCase
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('post')->once()
             ->with('/2010-04-01/Accounts/AC123/Messages.json', $this->formHeaders,
-                'From=%2B1222&To=%2B44123&MediaUrls=http%3A%2F%2Fexample.com%2Fimage1&MediaUrls=http%3A%2F%2Fexample.com%2Fimage2')
+                'From=%2B1222&To=%2B44123&MediaUrl=http%3A%2F%2Fexample.com%2Fimage1&MediaUrl=http%3A%2F%2Fexample.com%2Fimage2')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array('sid' => 'SM123'))
             ));
         $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
         $msg = $client->account->messages->create('+1222', '+44123',
-            array('MediaUrls' => array('http://example.com/image1', 'http://example.com/image2')));
+            array('MediaUrl' => array('http://example.com/image1', 'http://example.com/image2')));
         $this->assertSame('SM123', $msg->sid);
     }
 
