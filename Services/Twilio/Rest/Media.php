9f0e050a30b9f18a915691d7c51f2de30c8ec934
Kevin Burke <kevin@twilio.com>
2013-09-03 15:42:04 -0700

    one more link

--- Services/Twilio/Rest/Media.php
+++ Services/Twilio/Rest/Media.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * A list of Media objects. For the definitive reference, see the `Twilio Media
- * List Documentation <https://www.twilio.com/docs/api/rest/media>`_.
+ * A list of :php:class:`Media <Services_Twilio_Rest_MediaInstance>` objects.
+ * For the definitive reference, see the `Twilio Media List Documentation
+ * <https://www.twilio.com/docs/api/rest/media>`_.
  */
 class Services_Twilio_Rest_Media extends Services_Twilio_ListResource {
 
