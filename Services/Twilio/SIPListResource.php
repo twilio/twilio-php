37ac777b1a5eec8d69b472c7b35617686b5cc292
Kevin Burke <kevin@twilio.com>
2013-09-06 11:34:37 -0400

    Typos

--- Services/Twilio/SIPListResource.php
+++ Services/Twilio/SIPListResource.php
@@ -4,8 +4,7 @@
  * This subclass of ListResource is used solely to update
  * the URI for sip resources.
  */
-abstract class Service_Twilio_SIPListResource extends Services_Twilio_Resource_ListResource
-{
+abstract class Services_Twilio_SIPListResource extends Services_Twilio_ListResource {
     public function __construct($client, $uri) {
         // Rename all /Sip/ uris to /SIP/
         $uri = preg_replace("#/Sip#", "/SIP", $uri);
