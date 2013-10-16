41187e2e6da820f61876a631b692b9631ee08168
Doug Black <dblack@twilio.com>
2013-10-16 14:45:09 -0700

    add number types to IncomingPhoneNumbers

--- Services/Twilio/Rest/IncomingPhoneNumbers.php
+++ Services/Twilio/Rest/IncomingPhoneNumbers.php
@@ -3,8 +3,44 @@
 class Services_Twilio_Rest_IncomingPhoneNumbers
     extends Services_Twilio_ListResource
 {
+
+    public function getLocal() {
+        $curried = new Services_Twilio_PartialApplicationHelper();
+        $curried->set(
+            'getList',
+            array($this, 'getList'),
+            array('Local')
+        );
+        return $curried;
+    }
+
+    public function getTollFree() {
+        $curried = new Services_Twilio_PartialApplicationHelper();
+        $curried->set(
+            'getList',
+            array($this, 'getList'),
+            array('TollFree')
+        );
+        return $curried;
+    }
+
+    public function getMobile() {
+        $curried = new Services_Twilio_PartialApplicationHelper();
+        $curried->set(
+            'getList',
+            array($this, 'getList'),
+            array('Mobile')
+        );
+        return $curried;
+    }
+
     function create(array $params = array()) {
         return parent::_create($params);
+    }
+
+    function getList($type, array $params = array())
+    {
+        return $this->client->retrieveData($this->uri . "/$type", $params);
     }
 
     /**
@@ -12,10 +48,10 @@ class Services_Twilio_Rest_IncomingPhoneNumbers
      * than one number matches the search string, returns the first one.
      *
      * @param string $number The number in E.164 format, eg "+684105551234"
-     * @return Services_Twilio_Rest_IncomingPhoneNumber|null The number object, 
+     * @return Services_Twilio_Rest_IncomingPhoneNumber|null The number object,
      *      or null
-     * 
-     * @throws Services_Twilio_RestException if the number is invalid, not 
+     *
+     * @throws Services_Twilio_RestException if the number is invalid, not
      *      provided in E.164 format or for any other API exception.
      */
     public function getNumber($number) {
--- tests/resources/IncomingPhoneNumbersTest.php
+++ tests/resources/IncomingPhoneNumbersTest.php
@@ -46,5 +46,54 @@ class IncomingPhoneNumbersTest extends PHPUnit_Framework_TestCase {
         $number = $client->account->incoming_phone_numbers->getNumber('+14105551234');
         $this->assertNull($number);
     }
+
+    function testGetMobile() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/IncomingPhoneNumbers/Mobile.json')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('incoming_phone_numbers' => array(
+                    'friendly_name' => '(510) 564-7903'
+                )))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $nums = $client->account->incoming_phone_numbers->getMobile()->getList();
+        foreach ($nums as $num) {
+            $this->assertEquals('(510) 564-7903', $num->friendly_name);
+        }
+    }
+
+    function testGetLocal() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/IncomingPhoneNumbers/Local.json')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('incoming_phone_numbers' => array(
+                    'friendly_name' => '(510) 564-7903'
+                )))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $nums = $client->account->incoming_phone_numbers->getLocal()->getList();
+        foreach ($nums as $num) {
+            $this->assertEquals('(510) 564-7903', $num->friendly_name);
+        }
+    }
+
+    function testGetTollFree() {
+        $http = m::mock(new Services_Twilio_TinyHttp);
+        $http->shouldReceive('get')->once()
+            ->with('/2010-04-01/Accounts/AC123/IncomingPhoneNumbers/TollFree.json')
+            ->andReturn(array(200, array('Content-Type' => 'application/json'),
+                json_encode(array('incoming_phone_numbers' => array(
+                    'friendly_name' => '(510) 564-7903'
+                )))
+            ));
+        $client = new Services_Twilio('AC123', '123', '2010-04-01', $http);
+        $nums = $client->account->incoming_phone_numbers->getTollFree()->getList();
+        foreach ($nums as $num) {
+            $this->assertEquals('(510) 564-7903', $num->friendly_name);
+        }
+    }
+
 }
 
