d0bf14d8429c630ecde7c8e6823adfe56428f259
Keith Casey <keith@caseysoftware.com>
2014-12-03 17:12:45 -0600

    moving the delete() method from the base class to the Call and Message classes

--- Services/Twilio/InstanceResource.php
+++ Services/Twilio/InstanceResource.php
@@ -33,16 +33,6 @@ abstract class Services_Twilio_InstanceResource extends Services_Twilio_Resource
     }
 
     /**
-     * Make a request to delete the specified resource.
-     *
-     * :rtype: boolean
-     */
-    public function delete()
-    {
-        return $this->client->deleteData($this->uri);
-    }
-
-    /**
      * Add all properties from an associative array (the JSON response body) as
      * properties on this instance resource, except the URI
      *
