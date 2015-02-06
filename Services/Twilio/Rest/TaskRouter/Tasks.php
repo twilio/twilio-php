62d8129668d5a4fc0d08a24917877c821e271430
Alexandre Payment <apayment@twilio.com>
2015-02-06 14:10:43 -0800

    Add new timeout optional argument and field for task

--- Services/Twilio/Rest/TaskRouter/Tasks.php
+++ Services/Twilio/Rest/TaskRouter/Tasks.php
@@ -2,7 +2,7 @@
 
 class Services_Twilio_Rest_TaskRouter_Tasks extends Services_Twilio_TaskRouterListResource {
 
-    public function create($attributes, $workflowSid) {
+    public function create($attributes, $workflowSid, array $params = array()) {
         $params['Attributes'] = $attributes;
         $params['WorkflowSid'] = $workflowSid;
         return parent::_create($params);
--- tests/resources/task_router/TasksTest.php
+++ tests/resources/task_router/TasksTest.php
@@ -10,12 +10,14 @@ class TasksTest extends PHPUnit_Framework_TestCase
         $http->shouldReceive('post')->once()
             ->with('/v1/Accounts/AC123/Workspaces/WS123/Tasks.json',
                 array('Content-Type' => 'application/x-www-form-urlencoded'),
-                'Attributes=attribute&WorkflowSid=WF123')
+                'Timeout=60&Attributes=attribute&WorkflowSid=WF123')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array('sid' => 'WT123'))
             ));
         $taskrouterClient = new TaskRouter_Services_Twilio('AC123', '123', 'WS123', 'v1', $http);
-        $activity = $taskrouterClient->workspace->tasks->create('attribute', 'WF123');
+        $activity = $taskrouterClient->workspace->tasks->create('attribute', 'WF123',array(
+            'Timeout' => 60,
+        ));
         $this->assertNotNull($activity);
     }
 
