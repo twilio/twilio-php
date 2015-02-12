eec8098b73e645c469f59324db08ceb2c0b49741
Alexandre Payment <apayment@twilio.com>
2015-02-12 10:49:50 -0800

    Fix tests for next gen paging

--- tests/resources/task_router/EventsTest.php
+++ tests/resources/task_router/EventsTest.php
@@ -8,7 +8,7 @@ class EventsTest extends PHPUnit_Framework_TestCase
     function testGet() {
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('get')->once()
-            ->with('/v1/Accounts/AC123/Workspaces/WS123/Events/EV123.json')
+            ->with('/v1/Workspaces/WS123/Events/EV123')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array('sid' => 'EV123', 'description' => 'Test Worker'))
             ));
@@ -22,17 +22,12 @@ class EventsTest extends PHPUnit_Framework_TestCase
     {
         $http = m::mock(new Services_Twilio_TinyHttp);
         $http->shouldReceive('get')->once()
-            ->with('/v1/Accounts/AC123/Workspaces/WS123/Events.json?Page=0&PageSize=50')
+            ->with('/v1/Workspaces/WS123/Events?Page=0&PageSize=50')
             ->andReturn(array(200, array('Content-Type' => 'application/json'),
                 json_encode(array(
-                    'meta' => array('key' => 'events'),
+                    'meta' => array('key' => 'events', 'next_page_url' => null),
                     'events' => array(array('sid' => 'EV123'))
                 ))
-            ));
-        $http->shouldReceive('get')->once()
-            ->with('/v1/Accounts/AC123/Workspaces/WS123/Events.json?Page=1&PageSize=50')
-            ->andReturn(array(400, array('Content-Type' => 'application/json'),
-                '{"status":400,"message":"foo", "code": "20006"}'
             ));
         $taskrouterClient = new TaskRouter_Services_Twilio('AC123', '123', 'WS123', 'v1', $http);
         foreach ($taskrouterClient->workspace->events->getIterator(0, 50) as $event) {
--- tests/resources/task_router/WorkspacesTest.php
+++ tests/resources/task_router/WorkspacesTest.php
@@ -31,11 +31,6 @@ class WorkspacesTest extends PHPUnit_Framework_TestCase
                     'workspaces' => array(array('sid' => 'WS123'))
                 ))
             ));
-        $http->shouldReceive('get')->once()
-            ->with('/v1/Workspaces?Page=1&PageSize=50')
-            ->andReturn(array(400, array('Content-Type' => 'application/json'),
-                '{"status":400,"message":"foo", "code": "20006"}'
-            ));
         $taskrouterClient = new TaskRouter_Services_Twilio('AC123', '123', 'WS123', 'v1', $http);
         $this->assertNotNull($taskrouterClient->workspaces);
         foreach ($taskrouterClient->workspaces->getIterator(0, 50) as $workspace) {
