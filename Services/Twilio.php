904f5cba632de4530d89a26b98c7489d63598921
Alexandre Payment <apayment@twilio.com>
2015-04-07 15:46:33 -0700

    Implement monitor

--- Services/Twilio.php
+++ Services/Twilio.php
@@ -8,7 +8,9 @@
 
 function Services_Twilio_autoload($className)
 {
-    if (substr($className, 0, 15) != 'Services_Twilio' && substr($className, 0, 26) != 'TaskRouter_Services_Twilio') {
+    if (substr($className, 0, 15) != 'Services_Twilio' &&
+        substr($className, 0, 23) != 'Monitor_Services_Twilio' &&
+        substr($className, 0, 26) != 'TaskRouter_Services_Twilio') {
         return false;
     }
     $file = str_replace('_', '/', $className);
@@ -530,5 +532,76 @@ class Lookups_Services_Twilio extends Base_Services_Twilio
 		}
 		return $query_path;
 	}
+
+}
+
+/**
+ * Create a client to talk to the Twilio Monitor API.
+ *
+ *
+ * :param string               $sid:      Your Account SID
+ * :param string               $token:    Your Auth Token from `your dashboard
+ *      <https://www.twilio.com/user/account>`_
+ * :param string               $version:  API version to use
+ * :param $_http:    A HTTP client for making requests.
+ * :type $_http: :php:class:`Services_Twilio_TinyHttp`
+ * :param int                  $retryAttempts:
+ *      Number of times to retry failed requests. Currently only idempotent
+ *      requests (GET's and DELETE's) are retried.
+ *
+ * Here's an example:
+ *
+ * .. code-block:: php
+ *
+ *      require('Services/Twilio.php');
+ *      $client = new Monitor_Services_Twilio('AC123', '456bef', null, null, 3);
+ *      // Take some action with the client, etc.
+ */
+class Monitor_Services_Twilio extends Base_Services_Twilio
+{
+    protected $versions = array('v1');
+
+    public function __construct(
+        $sid,
+        $token,
+        $version = null,
+        Services_Twilio_TinyHttp $_http = null,
+        $retryAttempts = 1
+    )
+    {
+        parent::__construct($sid, $token, $version, $_http, $retryAttempts);
+
+        $this->events = new Services_Twilio_Rest_Monitor_Events($this, "/{$this->version}/Events");
+    }
+
+    /**
+     * Construct a URI based on initial path, query params, and paging
+     * information
+     *
+     * We want to use the query params, unless we have a next_page_uri from the
+     * API.
+     *
+     * :param string $path: The request path (may contain query params if it's
+     *      a next_page_uri)
+     * :param array $params: Query parameters to use with the request
+     * :param boolean $full_uri: Whether the $path contains the full uri
+     *
+     * :return: the URI that should be requested by the library
+     * :returntype: string
+     */
+    public static function getRequestUri($path, $params, $full_uri = false)
+    {
+        if (!$full_uri && !empty($params)) {
+            $query_path = $path . '?' . http_build_query($params, '', '&');
+        } else {
+            $query_path = $path;
+        }
+        return $query_path;
+    }
+
+    protected function _getBaseUri()
+    {
+        return 'https://monitor.twilio.com';
+    }
 
 }
