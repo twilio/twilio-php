fada492a050fb05f473092dba89dfd3affb6968a
Sam Kimbrel <skimbrel@twilio.com>
2015-04-08 11:06:54 -0700

    Add create() method to Addresses

--- Services/Twilio/Rest/Addresses.php
+++ Services/Twilio/Rest/Addresses.php
@@ -3,4 +3,19 @@
 class Services_Twilio_Rest_Addresses
     extends Services_Twilio_ListResource
 {
+
+	public function create($customerName, $street, $city, $region, $postalCode, $isoCountry,$friendlyName = null) {
+		$params = array(
+				'CustomerName' => $customerName,
+				'Street' => $street,
+				'City' => $city,
+				'Region' => $region,
+				'PostalCode' => $postalCode,
+				'IsoCountry' => $isoCountry,
+		);
+		if (!is_null($friendlyName)) {
+			$params['FriendlyName'] = $friendlyName;
+		}
+		return parent::_create($params);
+	}
 }
