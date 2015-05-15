27d304f443b01421c1e5049f5a291cfaa6686bc9
Senthil Ramakrishnan <senthgit@gmail.com>
2015-05-15 16:12:14 -0700

    Restricted Signing Key List Operation

--- Services/Twilio/Rest/SigningKeys.php
+++ Services/Twilio/Rest/SigningKeys.php
@@ -19,4 +19,31 @@ class Services_Twilio_Rest_SigningKeys extends Services_Twilio_ListResource
     {
         return parent::_create($params);
     }
+
+    /**
+     * Overriding to throw a runtime exception as this is not allowed to be used yet.
+     */
+    public function getPage(
+        $page = 0, $size = 50, $filters = array(), $deep_paging_uri = null
+    ) {
+        throw new RuntimeException("SigningKeys List is not supported");
+    }
+
+    /**
+     * Overriding to throw a runtime exception as this is not allowed to be used yet.
+     */
+    public function getIterator(
+        $page = 0, $size = 50, $filters = array()
+    ) {
+        throw new RuntimeException("Cannot retrieve an iterator on SigningKeys List");
+    }
+
+    /**
+     * Overriding to throw a runtime exception as this is not allowed to be used yet.
+     */
+    public function getPageGenerator(
+        $page, $size, $filters = array(), $deep_paging_uri = null
+    ) {
+        throw new RuntimeException("Cannot retrieve a Page Generator on SigningKeys List");
+    }
 }
