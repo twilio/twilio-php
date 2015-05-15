42a6e2ab98719f3fc1627670491904e2f79fca2b
matt <matt@twilio.com>
2015-05-15 00:14:21 -0400

    Adding addRestGrant and enableNTS and tests

--- tests/AccessTokenTest.php
+++ tests/AccessTokenTest.php
@@ -59,4 +59,34 @@ class AccessTokenTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('sip:bob@AC123.endpoint.twilio.com', $grant->res);
         $this->assertEquals(array('listen', 'invite'), $grant->act);
     }
+
+    function testRestGrant()
+    {
+        $scat = new Services_Twilio_AccessToken(self::SIGNING_KEY_SID, self::ACCOUNT_SID, 'secret');
+        $scat->addRestGrant('/Apps');
+        $token = $scat->asJWT();
+        $this->assertNotNull($token);
+        $payload = JWT::decode($token, 'secret');
+        $this->validateClaims($payload);
+        $this->assertEquals(1, count($payload->grants));
+        $grant = $payload->grants[0];
+        $this->assertEquals('https://api.twilio.com/2010-04-01/Accounts/AC123/Apps', $grant->res);
+        $this->assertEquals(array('*'), $grant->act);
+    }
+
+    function testEnableNTS()
+    {
+        $scat = new Services_Twilio_AccessToken(self::SIGNING_KEY_SID, self::ACCOUNT_SID, 'secret');
+        $scat->enableNTS();
+        $token = $scat->asJWT();
+        $this->assertNotNull($token);
+        $payload = JWT::decode($token, 'secret');
+        $this->validateClaims($payload);
+        $this->assertEquals(1, count($payload->grants));
+        $grant = $payload->grants[0];
+        $this->assertEquals('https://api.twilio.com/2010-04-01/Accounts/AC123/Tokens', $grant->res);
+        $this->assertEquals(array('POST'), $grant->act);
+    }
+
+
 }
