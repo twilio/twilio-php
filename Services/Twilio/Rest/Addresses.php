0a0fa0ec5ae3337bc81b59134219e18c15786081
Katy Richard <katy@sharpspring.com>
2015-11-06 09:56:13 -0500

    Fix instance_name for Services_Twilio_Rest_Addresses class

--- Services/Twilio/Rest/Addresses.php
+++ Services/Twilio/Rest/Addresses.php
@@ -3,4 +3,10 @@
 class Services_Twilio_Rest_Addresses
     extends Services_Twilio_ListResource
 {
+
+    public function __construct($client, $uri) {
+        $this->instance_name = "Services_Twilio_Rest_Address";
+        parent::__construct($client, $uri);
+    }
+
 }
