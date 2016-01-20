22e380ae5fadffd9214561cac1a06b19e357a726
Carlos Diaz-Padron <cdpadron@twilio.com>
2016-01-19 16:09:47 -0800

    Invert req params on Address

--- Services/Twilio/Rest/Addresses.php
+++ Services/Twilio/Rest/Addresses.php
@@ -3,9 +3,14 @@
 class Services_Twilio_Rest_Addresses
     extends Services_Twilio_ListResource
 {
-    public function create($friendlyName, array $params = array())
+    public function create($customerName, $street, $city, $region, $postalCode, $isoCountry, array $params = array())
     {
-        $params["FriendlyName"] = $friendlyName;
+        $params["CustomerName"] = $customerName;
+        $params["Street"] = $street;
+        $params["City"] = $city;
+        $params["Region"] = $region;
+        $params["PostalCode"] = $postalCode;
+        $params["IsoCountry"] = $isoCountry;
 
         return parent::_create($params);
     }
