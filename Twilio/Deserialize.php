52547d9b74c7cd4e21f5c485a8171e471ba69ad9
matt <matt@twilio.com>
2016-02-25 14:29:20 -0800

    Exception is in the root namespace

--- Twilio/Deserialize.php
+++ Twilio/Deserialize.php
@@ -7,7 +7,7 @@ class Deserialize {
     public static function iso8601DateTime($s) {
         try {
             return new \DateTime($s, new \DateTimeZone('UTC'));
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return $s;
         }
     }
