33a249d274de93bbb6fb0bc0c928c8f6a95a1835
Jingming Niu <jniu@twilio.com>
2016-05-31 14:37:50 -0700

    Bump to 5.0.0-RC4

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = 5;
     const MINOR = 0;
-    const PATCH = '0-RC3';
+    const PATCH = '0-RC4';
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
