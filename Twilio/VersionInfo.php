e070fdb60f67c99bd470f0b7fe0b1370af9727f4
Jingming Niu <jniu@twilio.com>
2016-07-08 13:45:57 -0700

    Bump version

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = 5;
     const MINOR = 0;
-    const PATCH = '0-RC6';
+    const PATCH = '0-RC7';
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
