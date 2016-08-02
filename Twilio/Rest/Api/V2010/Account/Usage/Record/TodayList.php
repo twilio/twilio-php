40833ab099b16126eab2605e5be80deaa2981b15
matt <matt@twilio.com>
2016-08-02 11:52:26 -0700

    Add record filters to helper subresources

--- Twilio/Rest/Api/V2010/Account/Usage/Record/TodayList.php
+++ Twilio/Rest/Api/V2010/Account/Usage/Record/TodayList.php
@@ -41,6 +41,7 @@ class TodayList extends ListResource {
      * The results are returned as a generator, so this operation is memory
      * efficient.
      * 
+     * @param array $options Optional Arguments
      * @param int $limit Upper limit for the number of records to return. stream()
      *                   guarantees to never return more than limit.  Default is no
      *                   limit
@@ -54,10 +55,12 @@ class TodayList extends ListResource {
      *                        1000)
      * @return \Twilio\Stream stream of results
      */
-    public function stream($limit = null, $pageSize = null) {
+    public function stream(array $options = array(), $limit = null, $pageSize = null) {
         $limits = $this->version->readLimits($limit, $pageSize);
         
-        $page = $this->page($limits['pageSize']);
+        $page = $this->page(
+            $options, 
+        $limits['pageSize']);
         
         return $this->version->stream($page, $limits['limit'], $limits['pageLimit']);
     }
@@ -67,6 +70,7 @@ class TodayList extends ListResource {
      * Unlike stream(), this operation is eager and will load `limit` records into
      * memory before returning.
      * 
+     * @param array $options Optional Arguments
      * @param int $limit Upper limit for the number of records to return. read()
      *                   guarantees to never return more than limit.  Default is no
      *                   limit
@@ -80,21 +84,32 @@ class TodayList extends ListResource {
      *                        min(limit, 1000)
      * @return TodayInstance[] Array of results
      */
-    public function read($limit = null, $pageSize = Values::NONE) {
-        return iterator_to_array($this->stream($limit, $pageSize), false);
+    public function read(array $options = array(), $limit = null, $pageSize = Values::NONE) {
+        return iterator_to_array($this->stream(
+            $options, 
+        $limit, $pageSize), false);
     }
 
     /**
      * Retrieve a single page of TodayInstance records from the API.
      * Request is executed immediately
      * 
+     * @param array $options Optional Arguments
      * @param mixed $pageSize Number of records to return, defaults to 50
      * @param string $pageToken PageToken provided by the API
      * @param mixed $pageNumber Page Number, this value is simply for client state
      * @return \Twilio\Page Page of TodayInstance
      */
-    public function page($pageSize = Values::NONE, $pageToken = Values::NONE, $pageNumber = Values::NONE) {
+    public function page(array $options = array(), $pageSize = Values::NONE, $pageToken = Values::NONE, $pageNumber = Values::NONE) {
+        $options = new Values($options);
         $params = Values::of(array(
+            'Category' => $options['category'],
+            'StartDate<' => $options['startdateBefore'],
+            'StartDate' => $options['startDate'],
+            'StartDate>' => $options['startdateAfter'],
+            'EndDate<' => $options['enddateBefore'],
+            'EndDate' => $options['endDate'],
+            'EndDate>' => $options['enddateAfter'],
             'PageToken' => $pageToken,
             'Page' => $pageNumber,
             'PageSize' => $pageSize,
