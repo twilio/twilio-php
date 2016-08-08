43226dde30e04fde18550d9ba3bfcbbd54ee9609
matt <matt@twilio.com>
2016-08-08 15:34:24 -0700

    Bumping to version 5.0.0-RC19

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = 5;
     const MINOR = 0;
-    const PATCH = '0-RC18';
+    const PATCH = '0-RC19';
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
