a06f4dec9e87ff06d70ce30c52951803116f4c42
matt <matt@twilio.com>
2016-08-19 16:59:19 -0700

    Update for new alpha release

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = 5;
     const MINOR = 1;
-    const PATCH = '0-alpha1';
+    const PATCH = '0-alpha2';
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
