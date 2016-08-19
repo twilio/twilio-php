1b5fe25928f904846d58d914bc259dc9865ab9fd
matt <matt@twilio.com>
2016-08-19 16:39:25 -0700

    Update VersionInfo for release

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -6,8 +6,8 @@ namespace Twilio;
 
 class VersionInfo {
     const MAJOR = 5;
-    const MINOR = 0;
-    const PATCH = 3;
+    const MINOR = 1;
+    const PATCH = 0;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
