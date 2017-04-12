9a8fc241e8510a823452790a814eda2238f8c872
Evan Fossier <evan.fossier@gmail.com>
2017-04-12 13:23:45 -0700

    Add new room integration test

--- Twilio/Tests/Integration/Video/V1/RoomTest.php
+++ Twilio/Tests/Integration/Video/V1/RoomTest.php
@@ -142,6 +142,49 @@ class RoomTest extends HolodeckTestCase {
         $this->assertNotNull($actual);
     }
 
+    public function testReadWithStatusResponse() {
+        $this->holodeck->mock(new Response(
+            200,
+            '
+            {
+                "rooms": [
+                    {
+                        "sid": "RM4070b618362c1682b2385b1f9982833c",
+                        "status": "completed",
+                        "date_created": "2017-04-03T22:21:49Z",
+                        "date_updated": "2017-04-03T22:21:51Z",
+                        "account_sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                        "type": "peer-to-peer",
+                        "enable_turn": true,
+                        "unique_name": "RM4070b618362c1682b2385b1f9982833c",
+                        "status_callback": null,
+                        "status_callback_method": "POST",
+                        "start_time": null,
+                        "end_time": "2017-04-03T22:21:51Z",
+                        "duration": 2,
+                        "max_participants": 10,
+                        "record_participants_on_connect": false,
+                        "url": "https://video.twilio.com/v1/Rooms/RM4070b618362c1682b2385b1f9982833c"
+                    }
+                ],
+                "meta": {
+                    "page": 0,
+                    "page_size": 50,
+                    "first_page_url": "https://video.twilio.com/v1/Rooms?PageSize=50&Page=0",
+                    "previous_page_url": null,
+                    "url": "https://video.twilio.com/v1/Rooms?PageSize=50&Page=0",
+                    "next_page_url": null,
+                    "key": "rooms"
+                }
+            }
+            '
+        ));
+
+        $actual = $this->twilio->video->v1->rooms->read();
+
+        $this->assertNotNull($actual);
+    }
+
     public function testUpdateRequest() {
         $this->holodeck->mock(new Response(500, ''));
 
