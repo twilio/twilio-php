6e940122b8ff50e1cd35f9502832649cd73f2854
Jeremy McEntire <jmcentire@twilio.com>
2018-01-30 17:48:24 -0800

    Release 5.16.4

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = 5;
     const MINOR = 16;
-    const PATCH = 3;
+    const PATCH = 4;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
