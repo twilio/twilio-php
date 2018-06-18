5c7d532a6022fbc48979ee2416170578a175fcd7
Yannie Yip <yyip@twilio.com>
2018-06-18 14:49:36 -0700

    Use append instead of nest

--- Twilio/TwiML/TwiML.php
+++ Twilio/TwiML/TwiML.php
@@ -73,7 +73,7 @@ abstract class TwiML {
 	 * @param array $attributes XML attributes
 	 */
 	public function addChild($name, $value = null, $attributes) {
-		$this->nest(new GenericNode($name, $value, $attributes));
+		$this->append(new GenericNode($name, $value, $attributes));
 	}
 
 	public function addText($text) {
