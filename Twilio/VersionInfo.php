9984743a96015258a7fec3a382cd789001278710
Twilio CI <twilio-ci@twilio.com>
2018-09-21 17:35:30 -0400

    Release 5.22.0

--- Twilio/VersionInfo.php
+++ Twilio/VersionInfo.php
@@ -6,8 +6,8 @@ namespace Twilio;
 
 class VersionInfo {
     const MAJOR = 5;
-    const MINOR = 21;
-    const PATCH = 4;
+    const MINOR = 22;
+    const PATCH = 0;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
