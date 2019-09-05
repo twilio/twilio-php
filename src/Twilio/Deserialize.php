c094a8404435f7268537acc7ca0efab2947a5baf
childish-sambino <sharrison@twilio.com>
2019-09-05 13:00:52 -0500

    Prevent null date-time strings from deserializing to 'now' (#557)

--- src/Twilio/Deserialize.php
+++ src/Twilio/Deserialize.php
@@ -7,16 +7,19 @@ class Deserialize {
     /**
      * Deserialize a string date into a DateTime object
      *
-     * @param string $s A date or date and time, can be iso8601, rfc2822, 
+     * @param string $s A date or date and time, can be iso8601, rfc2822,
      *                  YYYY-MM-DD format.
      * @return \DateTime DateTime corresponding to the input string, in UTC time.
      */
     public static function dateTime($s) {
         try {
-            return new \DateTime($s, new \DateTimeZone('UTC'));
+            if ($s) {
+                return new \DateTime($s, new \DateTimeZone('UTC'));
+            }
         } catch (\Exception $e) {
-            return $s;
+            // no-op
         }
-    }
 
+        return $s;
+    }
 }
