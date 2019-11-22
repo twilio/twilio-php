246698fd22675a4cbc8d8c5a017b0cb8941b3de0
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2019-11-22 17:44:30 +0900

    fix: Corrected `Continue` in the same way

--- src/Twilio/Http/CurlClient.php
+++ src/Twilio/Http/CurlClient.php
@@ -42,7 +42,7 @@ class CurlClient implements Client {
             }
 
             $parts = \explode("\r\n\r\n", $response, 3);
-            list($head, $body) = ($parts[0] == 'HTTP/1.1 100 Continue'
+            list($head, $body) = (preg_match('/\AHTTP\/1.\d 100 Continue\Z/', $parts[0])
                             || preg_match('/\AHTTP\/1.\d 200 Connection established\Z/', $parts[0]))
                                ? array($parts[1], $parts[2])
                                : array($parts[0], $parts[1]);
