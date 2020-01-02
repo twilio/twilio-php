3010e02e8aaa21f4fc36ee75ec88ab5f90b3b7aa
Sam Harrison <sharrison@twilio.com>
2020-01-02 10:19:58 -0600

    chore!: remove deprecated code

--- src/Twilio/Jwt/Grants/VideoGrant.php
+++ src/Twilio/Jwt/Grants/VideoGrant.php
@@ -6,31 +6,7 @@ namespace Twilio\Jwt\Grants;
 
 class VideoGrant implements Grant {
 
-    private $configurationProfileSid;
     private $room;
-
-    /**
-     * Returns the configuration profile sid
-     *
-     * @return string the configuration profile sid
-     */
-    public function getConfigurationProfileSid() {
-        return $this->configurationProfileSid;
-    }
-
-    /**
-     * Set the configuration profile sid of the grant
-     * @deprecated in favor of setRoom/getRoom
-     *
-     * @param string $configurationProfileSid configuration profile sid of grant
-     *
-     * @return $this updated grant
-     */
-    public function setConfigurationProfileSid($configurationProfileSid) {
-        \trigger_error('Configuration profile sid is deprecated, use room instead.', E_USER_NOTICE);
-        $this->configurationProfileSid = $configurationProfileSid;
-        return $this;
-    }
 
     /**
      * Returns the room
@@ -58,7 +34,7 @@ class VideoGrant implements Grant {
      * @return string type of the grant
      */
     public function getGrantKey() {
-        return "video";
+        return 'video';
     }
 
     /**
@@ -68,9 +44,6 @@ class VideoGrant implements Grant {
      */
     public function getPayload() {
         $payload = array();
-        if ($this->configurationProfileSid) {
-            $payload['configuration_profile_sid'] = $this->configurationProfileSid;
-        }
         if ($this->room) {
             $payload['room'] = $this->room;
         }
