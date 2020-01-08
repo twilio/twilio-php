e1ea30f30e424ae76de01cfd8d582a5d29b13267
Twilio <twilio-ci@twilio.com>
2020-01-08 21:00:51 +0000

    Release 5.42.0

--- src/Twilio/VersionInfo.php
+++ src/Twilio/VersionInfo.php
@@ -6,8 +6,8 @@ namespace Twilio;
 
 class VersionInfo {
     const MAJOR = 5;
-    const MINOR = 41;
-    const PATCH = 1;
+    const MINOR = 42;
+    const PATCH = 0;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
