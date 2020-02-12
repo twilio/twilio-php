b4c6118250d5860790f87dff0aea7962ab831d5b
childish-sambino <sharrison@twilio.com>
2020-02-12 12:55:55 -0600

    fix: only pass query option to Guzzle client if it is provided (#602)

--- tests/Twilio/Unit/Jwt/ClientTokenTest.php
+++ tests/Twilio/Unit/Jwt/ClientTokenTest.php
@@ -12,33 +12,33 @@ class ClientTokenTest extends UnitTest {
     public function testNoPermissions() {
         $token = new ClientToken('AC123', 'foo');
         $payload = JWT::decode($token->generateToken(), 'foo');
-        $this->assertEquals($payload->iss, "AC123");
+        $this->assertEquals($payload->iss, 'AC123');
         $this->assertEquals($payload->scope, '');
     }
 
     public function testInboundPermissions() {
         $token = new ClientToken('AC123', 'foo');
-        $token->allowClientIncoming("andy");
+        $token->allowClientIncoming('andy');
         $payload = JWT::decode($token->generateToken(), 'foo');
 
-        $eurl = "scope:client:incoming?clientName=andy";
+        $eurl = 'scope:client:incoming?clientName=andy';
         $this->assertEquals($payload->scope, $eurl);
     }
 
     public function testOutboundPermissions() {
         $token = new ClientToken('AC123', 'foo');
-        $token->allowClientOutgoing("AP123");
-        $payload = JWT::decode($token->generateToken(), 'foo');;
-        $eurl = "scope:client:outgoing?appSid=AP123";
-        $this->assertContains($eurl, $payload->scope);
+        $token->allowClientOutgoing('AP123');
+        $payload = JWT::decode($token->generateToken(), 'foo');
+        $eurl = 'scope:client:outgoing?appSid=AP123';
+        $this->assertStringContainsString($eurl, $payload->scope);
     }
 
     public function testOutboundPermissionsParams() {
         $token = new ClientToken('AC123', 'foo');
-        $token->allowClientOutgoing("AP123", array("foobar" => 3));
+        $token->allowClientOutgoing('AP123', ['foobar' => 3]);
         $payload = JWT::decode($token->generateToken(), 'foo');
 
-        $eurl = "scope:client:outgoing?appSid=AP123&appParams=foobar%3D3";
+        $eurl = 'scope:client:outgoing?appSid=AP123&appParams=foobar%3D3';
         $this->assertEquals($payload->scope, $eurl);
     }
 
@@ -47,18 +47,18 @@ class ClientTokenTest extends UnitTest {
         $token->allowEventStream();
         $payload = JWT::decode($token->generateToken(), 'foo');
 
-        $event_uri = "scope:stream:subscribe?path=%2F2010"
-            . "-04-01%2FEvents&params=";
+        $event_uri = 'scope:stream:subscribe?path=%2F2010'
+            . '-04-01%2FEvents&params=';
         $this->assertEquals($payload->scope, $event_uri);
     }
 
     public function testEventsWithFilters() {
         $token = new ClientToken('AC123', 'foo');
-        $token->allowEventStream(array("foobar" => "hey"));
+        $token->allowEventStream(['foobar' => 'hey']);
         $payload = JWT::decode($token->generateToken(), 'foo');
 
-        $event_uri = "scope:stream:subscribe?path=%2F2010-"
-            . "04-01%2FEvents&params=foobar%3Dhey";
+        $event_uri = 'scope:stream:subscribe?path=%2F2010-'
+            . '04-01%2FEvents&params=foobar%3Dhey';
         $this->assertEquals($payload->scope, $event_uri);
     }
 
@@ -74,21 +74,21 @@ class ClientTokenTest extends UnitTest {
 
     public function testDecode() {
         $token = new ClientToken('AC123', 'foo');
-        $token->allowClientOutgoing("AP123", array("foobar"=> 3));
-        $token->allowClientIncoming("andy");
+        $token->allowClientOutgoing('AP123', ['foobar' => 3]);
+        $token->allowClientIncoming('andy');
         $token->allowEventStream();
 
-        $outgoing_uri = "scope:client:outgoing?appSid="
-            . "AP123&appParams=foobar%3D3&clientName=andy";
-        $incoming_uri = "scope:client:incoming?clientName=andy";
-        $event_uri = "scope:stream:subscribe?path=%2F2010-04-01%2FEvents";
+        $outgoing_uri = 'scope:client:outgoing?appSid='
+            . 'AP123&appParams=foobar%3D3&clientName=andy';
+        $incoming_uri = 'scope:client:incoming?clientName=andy';
+        $event_uri = 'scope:stream:subscribe?path=%2F2010-04-01%2FEvents';
 
         $payload = JWT::decode($token->generateToken(), 'foo');
         $scope = $payload->scope;
 
-        $this->assertContains($outgoing_uri, $scope);
-        $this->assertContains($incoming_uri, $scope);
-        $this->assertContains($event_uri, $scope);
+        $this->assertStringContainsString($outgoing_uri, $scope);
+        $this->assertStringContainsString($incoming_uri, $scope);
+        $this->assertStringContainsString($event_uri, $scope);
     }
 
 
@@ -112,7 +112,7 @@ class ClientTokenTest extends UnitTest {
     function zeroLengthNameInvalid() {
         $this->expectException('InvalidArgumentException');
         $token = new ClientToken('AC123', 'foo');
-        $token->allowClientIncoming("");
+        $token->allowClientIncoming('');
         $this->fail('exception should have been raised');
     }
 }
