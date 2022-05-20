7c75c9dc6da0e39ebfe10b3e10af4ed42cb48956
Steve Bauman <steven_bauman@outlook.com>
2022-05-20 09:11:43 -0400

    Fix `capabilities` property type

--- src/Twilio/Rest/Api/V2010/Account/IncomingPhoneNumberInstance.php
+++ src/Twilio/Rest/Api/V2010/Account/IncomingPhoneNumberInstance.php
@@ -23,7 +23,7 @@ use Twilio\Version;
  * @property string $addressRequirements
  * @property string $apiVersion
  * @property bool $beta
- * @property string $capabilities
+ * @property array $capabilities
  * @property \DateTime $dateCreated
  * @property \DateTime $dateUpdated
  * @property string $friendlyName
