687245ed07dc807eec94389f715323cf13c1e316
Twilio <team_interfaces+github@twilio.com>
2022-10-19 12:27:21 -0700

    Release 6.43.0

--- src/Twilio/VersionInfo.php
+++ src/Twilio/VersionInfo.php
@@ -6,8 +6,8 @@ namespace Twilio;
 
 class VersionInfo {
     const MAJOR = 6;
-    const MINOR = 42;
-    const PATCH = 2;
+    const MINOR = 43;
+    const PATCH = 0;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
