f55d5dbbe259deea6a88809477d244f319eaa832
Shubham Tiwari <stiwari@twilio.com>
2024-03-25 19:29:56 +0530

    chore: updated rc in VersionInfo.php

--- src/Twilio/VersionInfo.php
+++ src/Twilio/VersionInfo.php
@@ -5,9 +5,9 @@ namespace Twilio;
 
 
 class VersionInfo {
-    const MAJOR = "7";
-    const MINOR = "16";
-    const PATCH = "1";
+    const MAJOR = "8";
+    const MINOR = "0";
+    const PATCH = "0-rc.0";
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
