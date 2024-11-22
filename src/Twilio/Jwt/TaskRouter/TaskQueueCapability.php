784287864ff144c8d4be5003b067402fb91ad73e
Bruce Wells <brucekwells@gmail.com>
2024-11-21 22:15:54 -0500

    Parameters with a default of null must allow null type

--- src/Twilio/Jwt/TaskRouter/TaskQueueCapability.php
+++ src/Twilio/Jwt/TaskRouter/TaskQueueCapability.php
@@ -11,7 +11,7 @@ namespace Twilio\Jwt\TaskRouter;
  */
 class TaskQueueCapability extends CapabilityToken {
     public function __construct(string $accountSid, string $authToken, string $workspaceSid, string $taskQueueSid,
-                                string $overrideBaseUrl = null, string $overrideBaseWSUrl = null) {
+                                ?string $overrideBaseUrl = null, ?string $overrideBaseWSUrl = null) {
         parent::__construct($accountSid, $authToken, $workspaceSid, $taskQueueSid, null, $overrideBaseUrl, $overrideBaseWSUrl);
     }
 
