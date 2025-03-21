5ccc41e09d27bb498cdcb70bbcba4d67050427ee
Shubham Tiwari <stiwari@twilio.com>
2025-03-21 18:27:48 +0530

    chore: added unit tests for credential providers

--- tests/Twilio/Unit/CredentialProvider/ClientCredentialProviderTest.php
+++ tests/Twilio/Unit/CredentialProvider/ClientCredentialProviderTest.php
@@ -5,24 +5,28 @@ namespace CredentialProvider;
 
 use Twilio\AuthStrategy\TokenAuthStrategy;
 use Twilio\CredentialProvider\ClientCredentialProviderBuilder;
+use Twilio\Http\BearerToken\ApiTokenManager;
 use Twilio\Tests\Unit\UnitTest;
 
 class ClientCredentialProviderTest extends UnitTest {
     /*
      * @var ClientCredentialProvider
      */
+    private $clientCredentialProviderBuilder;
     private $clientCredentialProvider;
 
     public function setUp(): void {
         parent::setUp();
-        $this->clientCredentialProvider = (new ClientCredentialProviderBuilder())->setClientId("clientId")->setClientSecret("clientSecret")->build();
+        $this->clientCredentialProviderBuilder = (new ClientCredentialProviderBuilder())->setClientId("clientId")->setClientSecret("clientSecret");
     }
 
     public function testAuthTypeIsClientCredentials(): void {
+        $this->clientCredentialProvider = $this->clientCredentialProviderBuilder->setGrantType("client-credentials")->build();
         $this->assertEquals('client-credentials', $this->clientCredentialProvider->getAuthType());
     }
 
     public function testAuthStrategyIsTokenAuthStrategy(): void {
+        $this->clientCredentialProvider = $this->clientCredentialProviderBuilder->setTokenManager(new ApiTokenManager())->build();
         $this->assertInstanceOf(TokenAuthStrategy::class, $this->clientCredentialProvider->toAuthStrategy());
     }
 }
