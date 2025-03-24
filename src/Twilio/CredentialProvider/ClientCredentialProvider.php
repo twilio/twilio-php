0def13d91a73c036f99598bbb8f1c92fa9aa0cc9
Shubham Tiwari <stiwari@twilio.com>
2025-03-24 17:38:39 +0530

    docs: added inline docs for ClientCredentialProvider.php

--- src/Twilio/CredentialProvider/ClientCredentialProvider.php
+++ src/Twilio/CredentialProvider/ClientCredentialProvider.php
@@ -7,9 +7,19 @@ use Twilio\Exceptions\TwilioException;
 use Twilio\Http\BearerToken\ApiTokenManager;
 use Twilio\Http\BearerToken\TokenManager;
 
-
+/**
+ * Class ClientCredentialProvider
+ * Credential provider for OAuth in public apis
+ */
 class ClientCredentialProvider extends CredentialProvider {
+    /**
+     * @var array $options - array of params required for token api
+     */
     private $options;
+
+    /**
+     * @var TokenManager $tokenManager - handles fetching and refreshing of token
+     */
     private $tokenManager;
 
     public function __construct() {
@@ -40,7 +50,12 @@ class ClientCredentialProvider extends CredentialProvider {
         $this->tokenManager = $tokenManager;
     }
 
-
+    /**
+     * Magic method to get properties - returns the property if it exists in the options array
+     * @param string $name
+     * @return mixed value of the property
+     * @throws TwilioException
+     */
     public function __get(string $name)
     {
         if (array_key_exists($name, $this->options)) {
@@ -49,6 +64,13 @@ class ClientCredentialProvider extends CredentialProvider {
         throw new TwilioException('Unknown property ' . $name);
     }
 
+    /**
+     * Magic method to set properties - sets the value of the property if it exists in the options array
+     * @param string $name
+     * @param $value
+     * @return void
+     * @throws TwilioException
+     */
     public function __set(string $name, $value)
     {
         if (array_key_exists($name, $this->options)) {
@@ -58,6 +80,10 @@ class ClientCredentialProvider extends CredentialProvider {
         }
     }
 
+    /**
+     * Returns TokenAuthStrategy using ApiTokenManager
+     * @return AuthStrategy
+     */
     public function toAuthStrategy(): AuthStrategy {
         if ($this->tokenManager === null) {
             $this->tokenManager = new ApiTokenManager($this->options);
