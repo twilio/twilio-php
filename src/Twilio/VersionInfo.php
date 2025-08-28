d803b29b26daa88d41fb098e08f172f503a18755
Twilio <team_interfaces+github@twilio.com>
2025-08-28 11:49:46 +0000

    Release 8.7.2

--- src/Twilio/VersionInfo.php
+++ src/Twilio/VersionInfo.php
@@ -7,7 +7,7 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = "8";
     const MINOR = "7";
-    const PATCH = "1";
+    const PATCH = "2";
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
