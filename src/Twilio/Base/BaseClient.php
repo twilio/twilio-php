feb2c22e6b29151f7a5ffc8469b1541482fd4939
Manisha Singh <singhmanisha.250025@gmail.com>
2026-02-04 09:35:50 +0530

    fix: Regional API domain processing

--- src/Twilio/Base/BaseClient.php
+++ src/Twilio/Base/BaseClient.php
@@ -161,30 +161,6 @@ class BaseClient
             $authStrategy = $this->credentialProvider->toAuthStrategy();
         }
 
-        if( ($this->edge === null && $this->region !== null) || ($this->edge !== null && $this->region === null) )
-        {
-            trigger_error(' For regional processing, DNS is of format product.city.region.twilio.com; otherwise use product.twilio.com.', E_USER_DEPRECATED);
-        }
-        if ($this->edge === null && $this->region !== null) {
-            $regionMap = [
-                'au1' => 'sydney',
-                'br1' => 'sao-paulo',
-                'de1' => 'frankfurt',
-                'ie1' => 'dublin',
-                'jp1' => 'tokyo',
-                'jp2' => 'osaka',
-                'sg1' => 'singapore',
-                'us1' => 'ashburn',
-                'us2' => 'umatilla'
-            ];
-            if (array_key_exists($this->region, $regionMap)) {
-                trigger_error(' Setting default `Edge` for the provided `region`.', E_USER_DEPRECATED);
-                $this->edge = $regionMap[$this->region];
-            }
-            if( $this->edge === null )
-                $this->edge = '';
-        }
-
         if (!$authStrategy) {
             if (!$username) {
                 throw new ConfigurationException('username is required');
--- tests/Twilio/Unit/Rest/ClientTest.php
+++ tests/Twilio/Unit/Rest/ClientTest.php
@@ -282,41 +282,4 @@ class ClientTest extends UnitTest {
         $userAgentExtensions = array_slice(explode(" ",$userAgent),-count($expectedExtensions));
         $this->assertEquals($userAgentExtensions,$expectedExtensions);
     }
-
-
-    public function testEdgeIsSetFromRegionWhenEdgeIsNull(): void {
-        $client = new Client('username', 'password', null, 'au1');
-        $client->request('GET', 'https://api.twilio.com');
-        $this->assertEquals('sydney', $client->getEdge());
-    }
-
-    public function testEdgeIsSetFromRegionWhenRegionIsXyz(): void {
-        // Create a mock of the CurlClient
-        $mockHttpClient = $this->createMock(CurlClient::class);
-
-        // Define the expected behavior
-        $mockHttpClient->expects($this->once())
-            ->method('request')
-            ->with('GET', 'https://api.xyz.twilio.com', [], [])
-            ->willReturn(new Response(200, 'Mocked response'));
-        $client = new Client('username', 'password', null, 'xyz');
-        $client->setHttpClient($mockHttpClient);
-        $client->request('GET', 'https://api.twilio.com');
-        $this->assertEquals('',$client->getEdge());
-        $this->assertEquals('xyz', $client->getRegion());
-    }
-
-    public function testEdgeRemainsWhenBothEdgeAndRegionAreSet(): void {
-        $client = new Client('username', 'password', null, 'au1');
-        $client->request('GET', 'https://api.twilio.com');
-        $client->setEdge('custom-edge');
-        $this->assertEquals('custom-edge', $client->getEdge());
-    }
-
-    public function testEdgeRemainsWhenBothEdgeAndRegionAreSetInConstructor(): void {
-        $client = new Client('username', 'password', null, 'au1', null, ['TWILIO_EDGE' => 'custom-edge']);
-        $this->assertEquals('custom-edge', $client->getEdge());
-        $this->assertEquals('au1', $client->getRegion());
-
-    }
 }
