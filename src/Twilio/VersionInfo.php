1c957b4e73fa6f7d67c6b88b0d2dac5095c5ca2e
sbansla <104902068+sbansla@users.noreply.github.com>
2026-08-14 16:51:13 +0530

    added rc version

--- src/Twilio/VersionInfo.php
+++ src/Twilio/VersionInfo.php
@@ -7,14 +7,9 @@ namespace Twilio;
 class VersionInfo {
     const MAJOR = "8";
     const MINOR = "0";
-    const PATCH = "0";
-    const PRERELEASE = "rc.2";
+    const PATCH = "0-rc.2";
 
     public static function string() {
-        $version = implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
-        if (self::PRERELEASE !== "") {
-            $version .= '-' . self::PRERELEASE;
-        }
-        return $version;
+        return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
     }
 }
